6fe7603ec2e630a29228db569d36a662c7f59f82
ShuShurochka <galyamova.alex@mail.ru>
2020-06-30 00:22:28 +0300

    Личный проект: надо подкачаться (часть 1)

--- js/map.js
+++ js/map.js
@@ -2,14 +2,28 @@
 // добавляет на страницу нужную карточку, отрисовывает метки и осуществляет взаимодействие карточки и метки на карте;
 'use strict';
 (function () {
+  window.map = {
+    successHandler: function (announcements) {
+      var pinFragment = document.createDocumentFragment();
+      for (var i = 0; i < announcements.length; i++) {
+        var announcement = window.pin.renderAnnouncementsPin(announcements[i]);
+        pinFragment.appendChild(announcement);
+      }
 
-  var pinFragment = document.createDocumentFragment();
+      var mapPins = document.querySelector('.map__pins');
+      mapPins.appendChild(pinFragment);
+    },
 
-  for (var i = 0; i < window.data.announcements.length; i++) {
-    var announcement = window.pin.renderAnnouncementsPin(window.data.announcements[i]);
-    pinFragment.appendChild(announcement);
-  }
+    errorHandler: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: orange;';
+      node.style.position = 'absolute';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '30px';
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
+    },
+  };
 
-  var mapPins = document.querySelector('.map__pins');
-  mapPins.appendChild(pinFragment);
 })();
